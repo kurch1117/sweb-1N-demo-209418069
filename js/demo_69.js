f5b0050293a4417bb12f87f841350fd8a92fb287
kurch1117 <zaq77998@gmail.com>
2022-10-13 21:30:26 +0800

    W06-P3: run 4 classdemo one by one

---
+++ js/demo_69.js
@@ -0,0 +1,18 @@
+function change (index) {
+    const p = document.querySelector('.show-classdemo');
+    console.log('p',p)
+    switch(index) {
+        case 1:
+        p.innerHTML = `<iframe src="./demo/w01_69/index.html" width="100%" height="100%" />`
+            break;
+        case 2:
+            p.innerHTML = `<iframe src="./demo/w02_69/card_69.html" width="100%" height="100%" />`
+            break;
+        case 3:
+            p.innerHTML = `<iframe src="./demo/w04_69/blog_69.html" width="100%" height="100%" />`
+            break;
+        case 4:
+            p.innerHTML = `<iframe src="./demo/w06_69/blog_69.html" width="100%" height="100%" />`
+            break;
+    }
+}
