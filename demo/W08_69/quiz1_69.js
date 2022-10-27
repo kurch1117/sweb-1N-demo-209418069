d97782bad6b7f2218a73da83239cf6da9b4030a4
kurch1117 <zaq77998@gmail.com>
2022-10-27 19:01:28 +0800

    w08-P1: navbar with card1 and card2

---
+++ demo/W08_69/quiz1_69.js
@@ -0,0 +1,13 @@
+function showPage (index) {
+    const p = document.querySelector('.show-quiz1-answer');
+    console.log('p',p)
+    switch(index) {
+        case 1:
+        p.innerHTML = `<iframe src="./cards/card_69.html" width="100%" height="100%" />`
+            break;
+        case 2:
+            p.innerHTML = `<iframe src="./cards/card2_69.html" width="100%" height="100%" />`
+            break;
+        
+    }
+}
