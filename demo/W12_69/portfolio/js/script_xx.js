e161c55cc7c6da1e2413042280027d22b79a8774
kurch1117 <zaq77998@gmail.com>
2022-11-24 19:49:15 +0800

    W12-P1: click nav-btn, add show-sidebar; click close-btn, remove show-sidebar

---
+++ demo/W12_69/portfolio/js/script_xx.js
@@ -0,0 +1,28 @@
+const navbar = document.querySelector('#nav');
+const date = document.querySelector('#date');
+
+const navBtn = document.querySelector('#nav-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('#sidebar');
+console.log('navBtn' , navBtn);
+console.log('closeBtn' , closeBtn);
+console.log('sidebar', sidebar);
+
+nav.addEventListener('click', function () {
+  sidebar.classList.add('show-sidebar');
+});
+
+closeBtn.addEventListener('click', function () {
+  sidebar.classList.remove('show-sidebar');
+})
+
+// add fixed class to navbar
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add('navbar-fixed');
+  } else {
+    navbar.classList.remove('navbar-fixed');
+  }
+});
+// set year
+
